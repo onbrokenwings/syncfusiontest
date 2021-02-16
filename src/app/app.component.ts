12acf630c6e05e490ea2bb800db9415d9bc67f97
Israel Llata García <illata@cic.es>
2021-02-16 17:53:49 +0100

    Cambios iniciales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ChartComponent } from '@syncfusion/ej2-angular-charts';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,71 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'syncfusion-test';
+
+  public primaryXAxis: Object = {
+    valueType: 'DateTime',
+    labelFormat: 'yy-MMM',
+    intervalType: 'Months',
+    majorGridLines: { width: 0 }
+  };
+
+  public primaryYAxis: Object = {
+    labelFormat: '{value} %',
+    rangePadding: 'None',
+    minimum: 50,
+    maximum: 90,
+    interval: 20,
+    lineStyle: { width: 1 },
+    majorGridLines: { width: 1 }
+  };
+
+  public marker: Object = {
+    visible: true,
+    height: 5,
+    width: 5
+  };
+
+  private count : number = 0;
+  starting: number = 60;
+  year: number = 1970;
+  public dataA01: Object[] = [];
+  interval : any;
+  @ViewChild('chart') public chart: ChartComponent;
+
+  constructor() {
+    this.dataA01 = this.getCompleteData();
+    this.getLiveData();
+  }
+  
+  getLiveData() {
+    this.interval = setInterval(() => {
+      let data = { x: new Date(this.year, (this.count + 1), this.count), y: this.getRandData() };
+      this.dataA01.push(data);
+      this.dataA01.shift();
+      console.log(`Generating data ... [${this.count}]: `, data);
+      this.count++;
+      this.chart.refresh();
+    }, 1000)
+  }
+
+  public getCompleteData(): any {
+    let serie: Object[] = [];
+    while (this.count < 50) {
+      serie.push({ x: new Date(this.year, (this.count + 1), this.count), y: this.getRandData() });
+      this.count++;
+    }
+    return serie;
+  }
+
+  private getRandData() {
+    let range = this.starting;
+    let rand = Math.floor(Math.random() * 20) + 1;
+    if (rand > .5) {
+      range += rand;
+    } else {
+      range -= rand;
+    }
+    return range;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DateTimePickerModule } from '@syncfusion/ej2-angular-calendars';
+import { AccumulationChartAllModule, ChartAllModule, RangeNavigatorAllModule } from '@syncfusion/ej2-angular-charts';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DateTimePickerModule,
+    ChartAllModule,
+    RangeNavigatorAllModule,
+    AccumulationChartAllModule
   ],
   providers: [],
   bootstrap: [AppComponent]
